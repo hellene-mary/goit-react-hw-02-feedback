6e162643dce84cf4e8dc69918be278862d5c152c
Grek Maria <grekmariaa@gmail.com>
2022-10-12 16:21:21 +0300

    is done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,63 @@
-import Feedback from './feedback/Feedback';
+import React, { Component } from 'react';
+import { FeedbackOptions } from './feedbackOptions/FeedbackOptions';
+import { Statistics } from 'components/statistics/Statistics';
+import { Section } from './section/Section';
+import { Notification } from './notification/Notification';
 
-export const App = () => {
-    return (
-        <>
-            <Feedback />
-        </>
-    );
-};
+class App extends Component {
+    state = {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+    };
+
+    onClickBtn = evt => {
+        this.setState(prevState => ({
+            [evt.target.name]: prevState[evt.target.name] + 1,
+        }));
+    };
+
+    countTotalFeedback() {
+        return this.state.good + this.state.neutral + this.state.bad;
+    }
+
+    countPositiveFeedbackPercentage() {
+        const total = this.countTotalFeedback();
+        return Math.round((this.state.good / total) * 100);
+    }
+
+    render() {
+        const total = this.countTotalFeedback();
+        const positivePercentage = this.countPositiveFeedbackPercentage();
+
+        return (
+            <>
+                <Section title="Please leave feedback!">
+                    <FeedbackOptions
+                        options={this.state}
+                        onLeaveFeedback={this.onClickBtn}
+                    />
+                </Section>
+
+                <Section title={'Statistics'}>
+                    {total === 0 ? (
+                        <Notification
+                            Notification
+                            message="There is no feedback"
+                        />
+                    ) : (
+                        <Statistics
+                            good={this.state.good}
+                            neutral={this.state.neutral}
+                            bad={this.state.bad}
+                            total={total}
+                            positivePercentage={positivePercentage}
+                        />
+                    )}
+                </Section>
+            </>
+        );
+    }
+}
+
+export default App;
--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,25 +1,30 @@
 import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
 
-export const Statistics = ({ good, neutral, bad, total, posFb }) => {
+export function Statistics({ good, neutral, bad, total, positivePercentage }) {
     return (
         <>
-            <h2 className={css.statisticsTitle}>Statistics</h2>
             <div>
-                <p>Good: {good}</p>
-                <p>Neutral: {neutral}</p>
-                <p>Bad: {bad}</p>
-                <p>Total: {total}</p>
-                <p>Positive feedback: {Number.isNaN(posFb) ? '0' : posFb}%</p>
+                <p className={css.statisticText}>Good: {good}</p>
+                <p className={css.statisticText}>Neutral: {neutral}</p>
+                <p className={css.statisticText}>Bad: {bad}</p>
+                <p className={css.statisticText}>Total: {total}</p>
+                <p className={css.statisticText}>
+                    Positive feedback:{' '}
+                    {Number.isNaN(positivePercentage)
+                        ? '0'
+                        : positivePercentage}
+                    %
+                </p>
             </div>
         </>
     );
-};
+}
 
 Statistics.propTypes = {
     good: PropTypes.number.isRequired,
     neutral: PropTypes.number.isRequired,
     bad: PropTypes.number.isRequired,
     total: PropTypes.number.isRequired,
-    posFb: PropTypes.number.isRequired,
+    positivePercentage: PropTypes.number.isRequired,
 };
